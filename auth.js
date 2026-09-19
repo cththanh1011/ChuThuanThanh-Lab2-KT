f1ba6b0e77a0846274f5ef0b09182e92a6903818
cththanh1011 <cththanh1011@gmail.com>
2026-09-19 10:59:58 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Check valid admin login
-    if (username === "admin" && password === "999") {
+    if (username === "admin" && password === "123") {
         return true;
     }
 
